f5f2e2a14fd58115b520efca62b7497443737cf4
weiyixiong <542111388@qq.com>
2015-10-16 07:03:46 +0800

    more simply

--- src/fastPower.java
+++ src/fastPower.java
@@ -5,39 +5,19 @@ public class fastPower {
 	 * @return: An integer
 	 */
 	int fastPower(int a, int b, int n) {
-		long res = 0;
-		if (a == 0 || b == 0)
-			return 0;
-		a = a % b;
-		if (a > 0) {
-			res = power(a, b, n);
-			if (n < 0) {
-				res = 1 / res;
-			}
-		} else {
-			res = power(a, b, n);
-			if (n % 2 == 1) {
-				res = 0 - res;
-			}
-			if (n < 0) {
-				res = 1 / res;
-			}
-		}
-		return (int) res % b;
+		if (n == 1) {
+            return a % b;
+        }
+        if (n == 0) {
+            return 1 % b;
+        }
+        
+        long product = fastPower(a, b, n / 2);
+        product = (product * product) % b;
+        if (n % 2 == 1) {
+            product = (product * a) % b;
+        }
+        return (int) product;
 	}
 
-	public long power(int a, int b, int n) {
-		if (n == 0) {
-			return 1;
-		}
-		if (n == 1) {
-			return a;
-		}
-		long result = power(a, b, n >> 1);
-		result = result * result % b;
-		if ((n & 1) == 1) {
-			result = result * a % b;
-		}
-		return result;
-	}
 }
