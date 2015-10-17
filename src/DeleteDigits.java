afdc8b3bc60e2aced9fee77914baeac59528f79f
weiyixiong <542111388@qq.com>
2015-10-17 22:31:31 +0800

    some

---
+++ src/DeleteDigits.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class DeleteDigits {
+	/**
+	 * @param A
+	 *            : A positive integer which has N digits, A is a string.
+	 * @param k
+	 *            : Remove k digits.
+	 * @return: A string
+	 */
+	public static String DeleteDigits(String A, int k) {
+		int len = A.length() - k;
+		String res = "";
+		return res;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(DeleteDigits("178542", 4));
+
+	}
+}
--- src/printfUtil.java
+++ src/printfUtil.java
@@ -5,4 +5,8 @@ public class printfUtil {
 	public static void printf(int[] nums) {
 		System.out.println(Arrays.toString(nums));
 	}
+
+	public static void printf(char[] nums) {
+		System.out.println(Arrays.toString(nums));
+	}
 }
