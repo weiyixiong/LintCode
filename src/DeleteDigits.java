a18abff41158262666d5c45a0c55b481deee2a52
weiyixiong <542111388@qq.com>
2015-10-18 23:31:22 +0800

    little

--- src/DeleteDigits.java
+++ src/DeleteDigits.java
@@ -11,11 +11,31 @@ public class DeleteDigits {
 	public static String DeleteDigits(String A, int k) {
 		int len = A.length() - k;
 		String res = "";
-		return res;
+		int start = 0;
+		int end = k + 1;
+		char min = '9' + 1;
+		while (res.length() < len) {
+			// System.out.println(start + " " + end);
+			for (int i = start; i < end; i++) {
+				if (min > A.charAt(i)) {
+					min = A.charAt(i);
+					start = i;
+				}
+			}
+			start++;
+			end++;
+			res += min;
+			min = '9' + 1;
+		}
+		start = 0;
+		while (res.charAt(start) == '0') {
+			start++;
+		}
+		return res.substring(start);
 	}
 
 	public static void main(String[] args) {
-		System.out.println(DeleteDigits("178542", 4));
-
+		System.out
+				.println(DeleteDigits("9876141516171818818181890001988181700198181778786761256512651653145345143", 55));
 	}
 }
