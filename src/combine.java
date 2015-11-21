e27d5de11a72fc5e1f17705e5dea06a2d4784b34
weiyixiong <542111388@qq.com>
2015-11-21 23:16:59 +0800

    little

---
+++ src/combine.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by winney on 15/11/21.
+ */
+public class combine {
+
+    public ArrayList<ArrayList<Integer>> combine(int n, int k) {
+        ArrayList<ArrayList<Integer>> rst = new ArrayList<ArrayList<Integer>>();
+        ArrayList<Integer> solution = new ArrayList<Integer>();
+
+        helper(rst, solution, n, k, 1);
+        return rst;
+    }
+
+    private void helper(
+            ArrayList<ArrayList<Integer>> rst,
+            ArrayList<Integer> solution,
+            int n,
+            int k,
+            int start) {
+
+        if (solution.size() == k) {
+            rst.add(new ArrayList(solution));
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            solution.add(i);
+
+            // the new start should be after the next number after i
+            helper(rst, solution, n, k, i + 1);
+            solution.remove(solution.size() - 1);
+        }
+    }
+}
