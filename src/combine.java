b9f448f68ded2c92601b8fd0978c083d3a459537
weiyixiong <542111388@qq.com>
2015-11-23 16:26:40 +0800

    little

--- src/combine.java
+++ src/combine.java
@@ -1,3 +1,5 @@
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -6,33 +8,50 @@ import java.util.List;
  * Created by winney on 15/11/21.
  */
 public class combine {
+    public static List<List<Integer>> combine2(int n, int k) {
 
-    public ArrayList<ArrayList<Integer>> combine(int n, int k) {
-        ArrayList<ArrayList<Integer>> rst = new ArrayList<ArrayList<Integer>>();
-        ArrayList<Integer> solution = new ArrayList<Integer>();
-
-        helper(rst, solution, n, k, 1);
-        return rst;
     }
 
-    private void helper(
-            ArrayList<ArrayList<Integer>> rst,
-            ArrayList<Integer> solution,
-            int n,
-            int k,
-            int start) {
-
-        if (solution.size() == k) {
-            rst.add(new ArrayList(solution));
-            return;
+    public static List<List<Integer>> combine(int n, int k) {
+        long size = n;
+        long size2 = k;
+        for (int i = 0; i < k - 1; i++) {
+            size *= n - i - 1;
+            size2 *= k - i - 1;
         }
-
-        for (int i = start; i <= n; i++) {
-            solution.add(i);
-
-            // the new start should be after the next number after i
-            helper(rst, solution, n, k, i + 1);
-            solution.remove(solution.size() - 1);
+        size /= size2;
+        if (size == 0) {
+            return null;
         }
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        List<Integer> cache = new ArrayList<Integer>(k);
+        for (int i = 1; i <= k; i++) {
+            cache.add(i);
+        }
+        int last = k - 1;
+        int index = 0;
+        int lastBak = last;
+        int indexBak = index;
+        for (int i = 0; i < size; i++) {
+            res.add(new ArrayList<>(cache));
+            cache.set(last, cache.get(last) + 1);
+            lastBak = last;
+            indexBak = index;
+            while (lastBak > 0 && cache.get(lastBak) > n - indexBak) {
+                lastBak--;
+                indexBak++;
+                cache.set(lastBak, cache.get(lastBak) + 1);
+            }
+            if (lastBak != last) {
+                while (lastBak < k - 1) {
+                    cache.set(lastBak + 1, cache.get(lastBak++) + 1);
+                }
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        combine(15, 14);
     }
 }
