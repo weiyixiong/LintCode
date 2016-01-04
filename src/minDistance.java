28aa06988ae26eb96ed121209ad76705f61c7645
weiyixiong <542111388@qq.com>
2016-01-04 15:54:38 +0800

    little

--- src/minDistance.java
+++ src/minDistance.java
@@ -8,15 +8,53 @@ public class minDistance {
      * @param word1 & word2: Two string.
      * @return: The minimum number of steps.
      */
+
+    public int minDistance(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+
+        int[][] dp = new int[n + 1][m + 1];
+        for (int i = 0; i < m + 1; i++) {
+            dp[0][i] = i;
+        }
+        for (int i = 0; i < n + 1; i++) {
+            dp[i][0] = i;
+        }
+
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1]));
+                }
+            }
+        }
+        return dp[n][m];
+    }
+
+
     static int maxLen;
 
-    public static int minDistance(String word1, String word2) {
-        maxLen = Math.max(word1.length(), word2.length());
+    public static int minDistance2(String word1, String word2) {
+        int minLen = Math.min(word1.length(), word2.length());
+        for (int i = 0; i < minLen; i++) {
+            int tmp = 0;
+            for (int j = 0; j < minLen; j++) {
+                while (j < minLen && i < minLen && word1.charAt(i) == word2.charAt(j)) {
+                    i++;
+                    j++;
+                    tmp++;
+                }
+            }
+            maxLen = Math.max(maxLen, tmp);
+        }
+        maxLen = word1.length() - maxLen + 5;
         return minDistanceHelper(word1, word2, 0);
     }
 
     public static int minDistanceHelper(String word1, String word2, int step) {
-        // System.out.println(word1 + "  " + word2);
         int min = maxLen;
         if (word1.equals(word2)) {
             return 0;
