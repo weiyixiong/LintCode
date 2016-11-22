513f6156b03c0d65720fd78f2689d4a8de52fcd9
weiyixiong <542111388@qq.com>
2016-11-22 19:11:19 +0800

    .

---
+++ src/firstWillWin.java
@@ -0,0 +1,12 @@
+/**
+ * Created by weiyixiong on 2016/11/22.
+ */
+public class firstWillWin {
+    /**
+     * @param n: an integer
+     * @return: a boolean which equals to true if the first player will win
+     */
+    public boolean firstWillWin(int n) {
+        return n % 3 != 0;
+    }
+}
